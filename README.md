23df41245b16250429d1531cd0f401b8a3885c64
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-06-27 22:44:01 -0700

    special-password-generator

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# special-password-generator
+# Special-password-generator
+This application shows the use of Javascript to generate a random & secure password for the user.
+
+#Description of application
+User will be prompted to enter a number between 8 & 128 to determine the length of the random password. Then the user will confirm for whether they want special characters, numbers, lowercase and uppercase. This data is needed to be collected in order to generate random password. Math.random & math.floor will then randomly select array data and generates random password on screen. 
+
+
+#Link for Application
+You can get this special password generator application running through this link ("")
+
+#Installing
+To install the application code, use GitHub's guidelines to clone the repository.
+
+
+
+#Summary 
+User will be presented with series of prompts and confirms yes or no & after data is collected, a special random password will be presented to you to copy.
+
+#Authors
+Robertson Rojas 
+
+#Acknowledgements 
+Shoutout to free code camp for providing a well explained Read.me to go off by & as always, mdn web docs for the great examples of syntaxes to go off by.
+
--- develop/script.js
+++ develop/script.js
@@ -1,22 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+var symList = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
+var lowercaseList = ["a", "b", "c" , "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercaseList = []
 
+var optionsBasket = []
+
+
+function randInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var random = Math.random()
+  return Math.floor(min*(1 - random) + random*max)
+}
+
+
+
+function randomSelect(list) {
+  return list[randInt(list.length)]
+}
 
 function generatePassword() {
  // console.log("This work!");
  // return "Generated Password";
  // Prompt the user for password;
-  var userInfo = prompt("Pick a number between 8 & 128 to generate password?")
+  var userInfo = prompt("Enter how long you want your password to be. (numbers only)")
 
   var wordLength = parseInt(userInfo)
 
   if (isNaN(wordLength)) {
     alert("Uh-oh, thats not a number!")
-    return
-  } else {
-    alert("Great, you picked a valid number!")
+    return null
   }
+
+  if (wordLength < 8 || wordLength > 128) {
+    alert("Length of password must be between 8 & 128 characters")
+    return 
+  }
+  
+  var userNum = confirm("Shall we include numbers in your password?") 
+  var userUppercase = confirm("Shall we include uppercase letters in your password?") 
+  var userSymbols = confirm("Shall we include symbols in your password?") 
+  var userLowercase = confirm("Shall we include lowercase in your password?") 
+
+
+
+  for (var i = 0; i < lowercaseList.length; i++) {
+    uppercaseList[i] = lowercaseList[i].toUpperCase()
+  }
+
+  if (userNum === true) {
+    optionsBasket.push(numList)
+  }
+
+  if (userUppercase === true) {
+    optionsBasket.push(uppercaseList)
+  }
+
+  if (userSymbols === true) {
+    optionsBasket.push(symList)
+  }
+
+  if (userLowercase === true) {
+    optionsBasket.push(lowercaseList)
+  }
+
+  if (optionsBasket.length === 0) {
+    optionsBasket.push(lowercaseList)
+  }
+
+  //console.log(optionsBasket)
+
+  var generatedCode = ""
+  
+  for (var i = 0; i < wordLength; i++) {
+    var specialList = randomSelect(optionsBasket)
+    var specialChar = randomSelect(specialList)
+    generatedCode += specialChar
+  }
+
+  //console.log(generatedCode)
+  return generatedCode 
+
 
 }
 
